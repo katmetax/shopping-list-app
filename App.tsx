03bf1929350608b7acd3358544fe5dce5e47c01e
Katerina Metaxopoulou <kat@katmetax.com>
2022-06-13 21:52:30 +0100

    Add NativeBase component library

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  NativeBaseProvider,
+  Container,
+  Heading,
+  VStack,
+  Center,
+  extendTheme,
+  Checkbox
+} from 'native-base';
+import { useState } from 'react';
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello world</Text>
-			<StatusBar style='auto' />
-		</View>
-	);
-}
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: 'dark'
+};
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
+// extend the theme
+const customTheme = extendTheme({ config });
+
+const App = () => {
+  const [groupValues, setGroupValues] = useState([]);
+  return (
+    <NativeBaseProvider theme={customTheme}>
+      <Center>
+        <Container>
+          <VStack space={4} alignItems="center">
+            <Heading color="black">My Shopping List</Heading>
+            <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3}>
+              <Checkbox.Group
+                onChange={setGroupValues}
+                value={groupValues}
+                accessibilityLabel="choose numbers"
+              >
+                <Checkbox value="one" my={2}>
+                  Milk
+                </Checkbox>
+              </Checkbox.Group>
+            </Center>
+            <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
+            <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
+          </VStack>
+        </Container>
+      </Center>
+    </NativeBaseProvider>
+  );
+};
+
+export default App;
--- README.md
+++ README.md
@@ -5,14 +5,25 @@ Finally a shopping list app that works and is decent.
 ## To-do
 ### Basic features
 [ ] Re-order items
+
 [ ] History of items added (even if checked off)
+
 [ ] Searchable list of items (checked off and not)
+
 [ ] Filter by checked/not checked
+
 [ ] Edit items
+
 [ ] Delete items
+
 [ ]Paste text and it will add each individual item separately
+
 
 ### Future features
 [ ] Dark/light theme
+
+[ ] Support multiple shopping lists
+
 [ ] Google assistant integration?
+
 [ ] Ability to share a link which goes to the site where you don’t have to have an account to access the list and edit it (or if you have the app it opens that)
